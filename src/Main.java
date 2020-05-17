d931de7706224ffd07eebf9a041be23829d3f24e
testsan <63858538+testsan@users.noreply.github.com>
2020-05-17 14:18:08 +0300

    code and README.md added

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketCost = 99;
+        int bonusMiles = ticketCost / 20;
+        System.out.println(bonusMiles);
+    }
+}
